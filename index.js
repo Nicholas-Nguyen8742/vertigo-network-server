f0b072e923953fe8287e87059b33d7684a656ae4
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-19 18:17:09 -0400

    Added a dotenv file for PORT holder and set up basic index.js file

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+const PORT = process.env.port || 8080;
+const pilotsRoute = require('./routes/pilotRoute');
+const clientRoute = require('./routes/clientRoute');
 
-
+app.use(cors());
 app.use(express.json());
 
-app.listen(8080, () => {
+
+// User Routes
+app.use('/pilots', pilotsRoute);
+app.use('/client', clientRoute);
+
+app.listen(PORT, () => {
     console.log("Vertigooo!!!");
 });
