7f4fe9c9b8c47c0cfa941ae3dc50f1a1aab28902
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-20 17:08:52 -0400

    Changed seed data due to how I imported data into SQL workbench

--- seeds_data/users.js
+++ seeds_data/users.js
@@ -1,125 +1,48 @@
 /* Unified Users w/ Type (Client or Pilot) */
 
 module.exports = [
-    {
-        id: 1,
-        type: "Pilot",
-        firstName: "Bail",
-        lastName: "Pate",
-        email: "bpate0@usa.gov",
-        city: "San Francisco",
-        state: "California",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/women/48.jpg"
+    { 
+        id: 1, 
+        type: "Pilot", 
+        firstName: "Wildon", 
+        lastName: "Canizares", 
+        city: "Tampa", 
+        state: "Florida", 
+        email: "wcanizares0@prlog.org", 
+        password: "yRU1hkO", 
+        profile: "https://randomuser.me/api/portraits/thumb/women/48.jpg" 
     },
-    {
-        id: 2,
-        type: "Pilot",
-        firstName: "Yul",
-        lastName: "Papierz",
-        email: "ypapierz1@dion.ne.jp",
-        city: "New York City",
-        state: "New York",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/36.jpg"
+    { 
+        id: 2, 
+        type: "Client", 
+        firstName: "Gwenora", 
+        lastName: "Mignot", 
+        city: "West Palm Beach", 
+        state: "Florida", 
+        email: "gmignot1@biblegateway.com", 
+        password: "TfDXAd7BeM9", 
+        profile: "https://randomuser.me/api/portraits/thumb/men/36.jpg" 
     },
-    {
-        id: 3,
-        type: "Pilot",
-        firstName: "Pryce",
-        lastName: "Ugoni",
-        email: "pugoni2@japanpost.jp",
-        city: "Sarasota",
-        state: "Florida",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
+    { 
+        id: 3, 
+        type: "Pilot", 
+        firstName: "Pearle", 
+        lastName: "Benaine", 
+        city: "Naples", 
+        state: "Florida", 
+        email: "pbenaine2@usa.gov", 
+        password: "lEQV3FMhQxE", 
         profile: "https://randomuser.me/api/portraits/thumb/women/14.jpg"
     },
-    {
-        id: 4,
-        type: "Pilot",
-        firstName: "Maureen",
-        lastName: "Roubottom",
-        email: "mroubottom3@mysql.com",
-        city: "Santa Clara",
-        state: "California",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
+    { 
+        id: 4, 
+        type: "Client", 
+        firstName: "Nonie", 
+        lastName: "Sedgeman", 
+        city: "Naples", 
+        state: "Florida", 
+        email: "nsedgeman3@auda.org.au", 
+        password: "lQaT8GES", 
         profile: "https://randomuser.me/api/portraits/thumb/women/14.jpg"
     },
-    {
-        id: 5,
-        type: "Pilot",
-        firstName: "Janelle",
-        lastName: "Frodsam",
-        email: "jfrodsam4@alibaba.com",
-        city: "Miami",
-        state: "Florida",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/women/30.jpg"
-    },
-    {
-        id: 6,
-        type: "Client",
-        firstName: "Axel",
-        lastName: "Lelande",
-        email: "alelande5@phpbb.com",
-        city: "Sacramento",
-        state: "California",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-    },
-    {
-        id: 7,
-        type: "Client",
-        firstName: "Carroll",
-        lastName: "Wernham",
-        email: "cwernham6@live.com",
-        city: "Washington",
-        state: "District of Columbia",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/89.jpg"
-    },
-    {
-        id: 8,
-        type: "Client",
-        firstName: "Aurie",
-        lastName: "Balk",
-        email: "abalk7@google.de",
-        city: "San Bernardino",
-        state: "California",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/80.jpg"
-    },
-    {
-        id: 9,
-        type: "Client",
-        firstName: "Fleming",
-        lastName: "Reimer",
-        email: "freimer8@lycos.com",
-        city: "El Paso",
-        state: "Texas",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/38.jpg"
-    },
-    {
-        id: 10,
-        type: "Client",
-        firstName: "Clemens",
-        lastName: "Featherstonhalgh",
-        email: "cfeatherstonhalgh9@ocn.ne.jp",
-        city: "Jacksonville",
-        state: "Florida",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/58.jpg"
-    },
-    {
-        id: 11,
-        type: "Client",
-        firstName: "Coletta",
-        lastName: "Willacot",
-        email: "cwillacota@bloomberg.com",
-        city: "Chicago",
-        state: "Illinois",
-        password: "219f0536aa16ccb2161658f6655a77c4074d6579",
-        profile: "https://randomuser.me/api/portraits/thumb/men/68.jpg"
-    },
-];
+]
