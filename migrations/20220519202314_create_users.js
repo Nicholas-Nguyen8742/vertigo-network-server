43d83a529d2741facd452c3cec1e6096652cad10
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-19 20:47:35 -0400

    Added post route for registration of users

--- migrations/20220519202314_create_users.js
+++ migrations/20220519202314_create_users.js
@@ -22,5 +22,5 @@ exports.up = function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = function(knex) {
-  return knex.schema.dropTable('pilots');
+  return knex.schema.dropTable('users');
 };
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -4,6 +4,38 @@ const knex = require('knex')(require('../knexfile'));
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// [ROUTE] - "/users/register"
+// [POST] - Creates new user through registration
+router.post('/register', (req, res) => {
+    const { firstName, lastName, email, type, city, state, password } = req.body;
+
+    if (!firstName || !lastName || !email || !type || !city || !state || !password) {
+        return res.status(400).send("Please enter the required fields.");
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 12);
+
+    const newUser = {
+        type: type,
+        firstName: firstName, 
+        lastName: lastName, 
+        email: email, 
+        city: city, 
+        state: state,
+        password: hashedPassword
+    };
+
+    knex('users')
+        .insert(newUser)
+        .then(() => {
+            res.status(201).send("Registered successfully");
+        })
+        .catch(() => {
+            res.status(400).send("Failed registration");
+        });
+});
+
+
 
 
 
