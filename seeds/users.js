5fd705f85f703bb3504c7174d6296993ccf8dd33
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-21 13:25:53 -0400

    Added seeds root to delete and insert data from top level table created

--- seeds/users.js
+++ seeds/users.js
@@ -1,11 +1,41 @@
 const userData = require('../seeds_data/users');
+const applicationsData = require('../seeds_data/applications');
+const missionsData = require('../seeds_data/missions');
+const portfolioData = require('../seeds_data/portfolio');
+const reviewsData = require('../seeds_data/reviews');
+
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
 exports.seed = function (knex) {
     return knex("users").del() 
-        .then(() => knex("users").insert(userData));
+        .then(() => knex("users").insert(userData))
+        .then(() => {
+            return knex('reviews').del();
+          })
+        .then(() => {
+            return knex('reviews').insert(reviewsData);
+        })
+        .then(() => {
+            return knex('missions').del();
+          })
+        .then(() => {
+            return knex('missions').insert(missionsData);
+        })
+        .then(() => {
+            return knex('applications').del();
+          })
+        .then(() => {
+            return knex('applications').insert(applicationsData);
+        })
+        .then(() => {
+            return knex('portfolio').del();
+          })
+        .then(() => {
+            return knex('portfolio').insert(portfolioData);
+        })
 };
 
 
