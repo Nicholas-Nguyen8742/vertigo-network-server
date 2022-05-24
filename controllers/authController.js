1f7bce11994e1e3658d175926469b0cd2099f987
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-05-23 22:24:14 -0400

    Sign-up & login works with front end, fixed missing piece for auth/current route

--- controllers/authController.js
+++ controllers/authController.js
@@ -76,13 +76,27 @@ exports.login = (req, res) => {
 // [ROUTE] - "/auth/current"
 // [GET] - Gets currently logged in user to validate JWT authentication
 exports.current = (authenticate, (req, res) => {
-    knex('users')
-        .where({ email: req.user.email })
-        .first()
-        .then((user) => {
-            // Respond with the user data
-            delete user.password;
-            res.json(user);
-        });
+    if (!req.headers.authorization) {
+        return res.status(401).send("Please login");
+    }
+
+    // Parse the Bearer token
+    const authToken = req.headers.authorization.split(" ")[1];
+    
+    // Verify the token
+    jwt.verify(authToken, process.env.JWT_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).send("Invalid auth token");
+        }
+
+        knex('users')
+            .where({ email: decoded.email })
+            .first()
+            .then((user) => {
+                // Respond with the user data
+                delete user.password;
+                res.json(user);
+            });
+    });
 });
 
